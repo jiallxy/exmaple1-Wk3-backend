4750f924183cd2808c0b2658d7f5140f6b055755
Justin Luo <87254766+jiallxy@users.noreply.github.com>
2025-10-22 20:23:38 -0400

    1

--- api/main.py
+++ api/main.py
@@ -28,6 +28,13 @@ facts = [
     "Octopuses have three hearts."
 ]
 
+@app.get("/")           #endpoint, or route, always starts with a forward slash
+def default_route():    #route handler function
+    """
+    This is the default endpoint for this back-end.
+    """
+    return "You have reached the default route. Back-end server is listening..."
+
 @app.get("/random-fact")
 async def get_random_fact():
     return {"fact": random.choice(facts)}
